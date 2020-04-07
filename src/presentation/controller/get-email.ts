6e6a584b297553786d261130dcdfb3dacbcb96fa
Washington Campos <washingtoncampos9@gmail.com>
2020-04-07 04:26:16 -0300

    feat: ensure GetEmailController returns 400 if no message is provided

--- src/presentation/controller/get-email.ts
+++ src/presentation/controller/get-email.ts
@@ -3,7 +3,7 @@ import { HttpRequest, HttpResponse } from '../protocols/http';
 
 export class GetEmailController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name || !httpRequest.body.email) {
+    if (!httpRequest.body.name || !httpRequest.body.email || !httpRequest.body.message) {
       return {
         statusCode: 400,
         body: {},
