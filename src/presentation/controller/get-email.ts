06fe95e3cb8b7cd38e49ff9b77e3111f86f0c811
Washington Campos <washingtoncampos9@gmail.com>
2020-04-07 04:48:30 -0300

    refactor: remove all if's and add a loop method

--- src/presentation/controller/get-email.ts
+++ src/presentation/controller/get-email.ts
@@ -4,25 +4,16 @@ import { MissingParamError } from '../errors/missing-param-error';
 
 export class GetEmailController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name'),
-      };
-    }
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email'),
-      };
-    }
-    if (!httpRequest.body.message) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('message'),
-      };
-    }
+    const requiredFields = ['name', 'email', 'message'];
 
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return {
+          statusCode: 400,
+          body: new MissingParamError(field),
+        };
+      }
+    }
     return { statusCode: 0, body: {} };
   }
 }
