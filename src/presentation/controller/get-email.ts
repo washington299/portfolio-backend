d2fd34481f498797f1cb7395c711844b09ce1db7
Washington Campos <washingtoncampos9@gmail.com>
2020-04-09 04:23:59 -0300

    feat: ensure GetEmailController throws if EmailValidator throws

--- src/presentation/controller/get-email.ts
+++ src/presentation/controller/get-email.ts
@@ -1,7 +1,7 @@
 /* eslint-disable consistent-return */
 import { HttpRequest, HttpResponse } from '../protocols/http';
-import { badRequest } from '../helpers/http-helpers';
-import { MissingParamError, InvalidParamError } from '../errors';
+import { badRequest, serverError } from '../helpers/http-helpers';
+import { MissingParamError, InvalidParamError, ServerError } from '../errors';
 import { EmailValidator } from '../protocols/email-validator';
 
 export class GetEmailController {
@@ -12,20 +12,24 @@ export class GetEmailController {
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'message'];
+    try {
+      const requiredFields = ['name', 'email', 'message'];
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
-    }
 
-    const { email } = httpRequest.body;
-    const isValid = this.emailValidator.isValid(email);
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'));
-    }
+      const { email } = httpRequest.body;
+      const isValid = this.emailValidator.isValid(email);
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'));
+      }
 
-    return { statusCode: 0, body: {} };
+      return { statusCode: 0, body: {} };
+    } catch (error) {
+      return serverError();
+    }
   }
 }
