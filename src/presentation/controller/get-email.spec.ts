0314419f73202246a7ec17ffad1bf474b917b8fd
Washington Campos <washingtoncampos9@gmail.com>
2020-04-09 03:19:52 -0300

    test: ensure GetEmailController returns 400 if an invalid email is provided

--- src/presentation/controller/get-email.spec.ts
+++ src/presentation/controller/get-email.spec.ts
@@ -1,13 +1,37 @@
 import { GetEmailController } from './get-email';
 import { MissingParamError } from '../errors/missing-param-error';
+import { InvalidParamError } from '../errors/invalid-param-error';
+import { EmailValidator } from '../protocols/email-validator';
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+
+  return new EmailValidatorStub();
+};
+
+interface SutTypes {
+  sut: GetEmailController;
+  emailValidatorStub: EmailValidator;
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidator();
+  const sut = new GetEmailController(emailValidatorStub);
+
+  return { sut, emailValidatorStub };
+};
 
 describe('Get Email from Client', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new GetEmailController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
-        email: 'valid_email',
-        mensage: 'valid_message',
+        email: 'valid_email@mail.com',
+        message: 'valid_message',
       },
     };
     const httpResponse = sut.handle(httpRequest);
@@ -16,11 +40,11 @@ describe('Get Email from Client', () => {
   });
 
   test('Should return 400 if no email is provided', () => {
-    const sut = new GetEmailController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         name: 'valid_name',
-        mensage: 'valid_message',
+        message: 'valid_message',
       },
     };
     const httpResponse = sut.handle(httpRequest);
@@ -29,15 +53,30 @@ describe('Get Email from Client', () => {
   });
 
   test('Should return 400 if no message is provided', () => {
-    const sut = new GetEmailController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         name: 'valid_name',
-        email: 'valid_email',
+        email: 'valid_email@mail.com',
       },
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('message'));
+  });
+
+  test('Should return 400 if invalid email is provided', () => {
+    const { sut, emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'invalid_email@mail.com',
+        message: 'valid_message',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'));
   });
 });
