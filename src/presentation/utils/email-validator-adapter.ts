31b9569a3fbac0ae67a05b8d6d6eeda36fff9ab0
Washington Campos <washingtoncampos9@gmail.com>
2020-04-19 03:37:20 -0300

    feat: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/presentation/utils/email-validator-adapter.ts
@@ -0,0 +1,8 @@
+import validor from 'validator';
+import { EmailValidator } from '../protocols/email-validator';
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid(email: string): boolean {
+    return validor.isEmail(email);
+  }
+}
